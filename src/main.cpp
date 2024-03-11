e22c614c0548218ce97c0e9b7114d0fab0d1fd30
lalugue <51893731+lalugue@users.noreply.github.com>
2024-03-11 17:13:36 +0800

    clear comments

--- src/main.cpp
+++ src/main.cpp
@@ -8,18 +8,15 @@
 
 Servo servo;
 
-// put function declarations here:
 void sweep();
 
 void setup()
 {
-  // put your setup code here, to run once:
   servo.attach(SERVO_PIN);
   servo.write(180);
 
   Serial.begin(115200);
 
-  // Start the receiver and if not 3. parameter specified, take LED_BUILTIN pin from the internal boards definition as default feedback LED
   IrReceiver.begin(IR_RECEIVE_PIN, ENABLE_LED_FEEDBACK);
 
   Serial.print(F("Ready to receive IR signals of protocols: "));
@@ -30,28 +27,20 @@ void loop()
 {
   if (IrReceiver.decode())
   {
-
-    /*
-     * Print a summary of received data
-     */
     if (IrReceiver.decodedIRData.protocol == UNKNOWN)
     {
       Serial.println(F("Received noise or an unknown (or not yet enabled) protocol"));
-      // We have an unknown protocol here, print extended info
       IrReceiver.printIRResultRawFormatted(&Serial, true);
-      IrReceiver.resume(); // Do it here, to preserve raw data for printing with printIRResultRawFormatted()
+      IrReceiver.resume();
     }
     else
     {
-      IrReceiver.resume(); // Early enable receiving of the next IR frame
+      IrReceiver.resume();
       IrReceiver.printIRResultShort(&Serial);
       IrReceiver.printIRSendUsage(&Serial);
     }
     Serial.println();
 
-    /*
-     * Finally, check the received data and perform actions according to the received command
-     */
     if (IrReceiver.decodedIRData.command && !(IrReceiver.decodedIRData.flags & IRDATA_FLAGS_IS_REPEAT))
     {
       Serial.println("Resetting targets");
